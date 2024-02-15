e3a51c7b57e0ba1f6bcf9b48589c66641def58d7
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2024-02-15 09:58:12 -0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace QualityOfLife
+{
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInDependency("ModCore")]
+	public class Plugin : BaseUnityPlugin
+	{
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+			SkipSplash();
+		}
+
+		private void SkipSplash()
+		{
+			// Load Main Menu
+			ModCore.Utility.LoadScene(1);
+		}
+	}
+}
